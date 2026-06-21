5fe7699186892cfeb2856e166b1a2fe8882b1163
Rajasekhar <rajaseg@gmail.com>
2026-06-21 21:43:09 +0530

    Updated few files

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="astro/client" />
+
 declare namespace App {
     interface Locals {
         pb: import('pocketbase').default;
-        user: import('pocketbase').AuthModel | null;
+        user: import('pocketbase').RecordModel | import('pocketbase').AdminModel | null;
     }
 }
