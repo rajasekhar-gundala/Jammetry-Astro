523cb9c9c809b2d18103edbb27e381cdaa1a55b2
Rajasekhar <rajaseg@gmail.com>
2026-06-21 21:45:21 +0530

    Reverted thechanges

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="astro/client" />
-
 declare namespace App {
     interface Locals {
         pb: import('pocketbase').default;
-        user: import('pocketbase').RecordModel | import('pocketbase').AdminModel | null;
+        user: import('pocketbase').AuthModel | null;
     }
 }
